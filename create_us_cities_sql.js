681dbd4a55f593b41182f9ca0ecc09c0e8e16ace
Jim Barrows <jim@reallybigtree.com>
2017-07-05 14:21:25 -0700

    Cities now get sql generated

---
+++ create_us_cities_sql.js
@@ -0,0 +1,32 @@
+const fs = require( 'fs' )
+const parse = require( 'csv-parse' );
+const transform = require( 'stream-transform' );
+const parser = parse( );
+const rs = fs.createReadStream( './etc/cities.csv' )
+const ws = fs.createWriteStream( './sql/05-us-cities.sql' );
+const uuidv4 = require( 'uuid/v4' );
+
+var transformer = transform( function( record, callback ) {
+	let city_uuid = uuidv4( );
+	let city_sql = "";
+	if ( record[1] === 'District of Columbia' ) {
+		city_sql = `
+  insert into geographic_boundary (id, name, geographic_boundary_type_id)
+    values ('${ city_uuid }', 'Washington D.C.', (select id from geographic_boundary_type where description = 'City' ));
+  insert into geographic_boundary_association (within_boundary, in_boundary)
+    values ( (select id from geographic_boundary where name='United States'), '${ city_uuid }');
+
+    `;
+	} else {
+		city_sql = `
+  insert into geographic_boundary (id, name, geographic_boundary_type_id)
+    values ('${ city_uuid }', '${ record[0] }', (select id from geographic_boundary_type where description = 'City' ));
+  insert into geographic_boundary_association (within_boundary, in_boundary)
+    values ( (select id from geographic_boundary where name='${ record[1] }'), '${ city_uuid }');
+
+    `;
+	}
+	callback( null, city_sql );
+}, { parallel: 10 });
+
+rs.pipe( parser ).pipe( transformer ).pipe( ws );
